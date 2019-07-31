386477fc742552ca8acd4f77c522c514ce509e2c
ggavryly <torgo121@gmail.com>
2019-07-31 19:27:13 +0300

    new name to include file

---
+++ include/doom_nukem.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   doom-nukem.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggavryly <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/31 19:21:08 by ggavryly          #+#    #+#             */
+/*   Updated: 2019/07/31 19:21:09 by ggavryly         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOOM_NUKEM_H
+# define DOOM_NUKEM_H
+# include <unistd.h>
+# include <errno.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <math.h>
+# include "libft.h"
+
+
+#endif
