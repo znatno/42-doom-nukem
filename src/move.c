f2b85bcefe9319b98cff707a748ccc321f34970d
znatno <ivanbohuna@gmail.com>
2019-08-01 18:53:05 +0300

    move.c raw prototype, upd

--- src/move.c
+++ src/move.c
@@ -4,10 +4,12 @@
 ** Сирі і неробочі функції для переміщення (поки що)
 **
 ** SDL_Rect — структура що містить в собі координати [x, y, w, h] вікна
+** offset - крок
 **
 ** Даний приклад був створений для переміщення картинки по вікну
 */
 
+/*
 void	move_forward(SDL_Renderer* render, SDL_Texture* texture, SDL_Rect &destrect, int offset = 5)
 {
 	destrect.y -= offset;
@@ -35,10 +37,13 @@ void	move_right(SDL_Renderer* render, SDL_Texture* texture, SDL_Rect &destrect, 
 	SDL_RenderClear(render);
 	SDL_RenderCopy(render, texture, NULL, &destrect);
 }
+*/
 
-void	render_update(SDL_Renderer* renderer, SDL_Texture* texture[], SDL_Rect* destrect[], int states[])
+void	render_update(t_sdl_main sdl, SDL_Texture* texture[], SDL_Rect* destrect[], int states[])
 {
-	SDL_RenderClear(render);
-	if(states[0]) SDL_RenderCopy(render, texture[0], NULL, destrect[0]);
-	if(states[1]) SDL_RenderCopy(render, texture[1], NULL, destrect[1]);
+	SDL_RenderClear(sdl.renderer);
+	if(states[0])
+		SDL_RenderCopy(sdl.renderer, texture[0], NULL, destrect[0]);
+	if(states[1])
+		SDL_RenderCopy(sdl.renderer, texture[1], NULL, destrect[1]);
 }
