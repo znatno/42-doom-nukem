abd5549d6089d59bfbc96d16a11957ce64c016f8
tal <prokish.tal@icloud.com>
2019-08-05 16:23:38 +0300

    deffs to funcs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,37 @@
 #include <AppleTextureEncoder.h>
 #include "sdl1.h"
 
+bool Overlap(float a0, float a1, float b0, float b1)
+{
+	if ((min(a0,a1) <= max(b0,b1) && min(b0,b1) <= max(a0,a1)))
+		return (true);
+	else
+		return (false);
+}
+
+float IntersectBox(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3)
+{
+	float result;
+	result = (Overlap(x0,x1,x2,x3) && Overlap(y0,y1,y2,y3));
+	return (result);
+}
+
+float PointSide(float px, float py, float x0, float y0, float x1, float y1)
+{
+	float result;
+	result = vxs((x1)-(x0), (y1)-(y0), (px)-(x0), (py)-(y0));
+	return (result);
+}
+
+t_xy Intersect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+	{
+		t_xy result;
+
+		result = (t_xy) {.x = vxs(vxs(x1,y1, x2,y2), (x1)-(x2), vxs(x3,y3, x4,y4), (x3)-(x4)) / vxs((x1)-(x2), (y1)-(y2), (x3)-(x4), (y3)-(y4)), \
+		.y = vxs(vxs(x1,y1, x2,y2), (y1)-(y2), vxs(x3,y3, x4,y4), (y3)-(y4)) / vxs((x1)-(x2), (y1)-(y2), (x3)-(x4), (y3)-(y4))};
+		return (result);
+	}
+
 /* VOPOLONC PART START */
 /* Define various vision related constants */
 
@@ -301,6 +332,7 @@ static void MovePlayer(t_player *plr, t_sector **sectors, float dx, float dy)
 		 	PointSide(px + dx, py + dy, vert[s + 0].x, vert[s + 0].y,
 					  vert[s + 1].x, vert[s + 1].y) < 0)
 		{
+			printf("%f", PointSide(px + dx, py + dy, vert[s + 0].x, vert[s + 0].y, vert[s + 1].x, vert[s + 1].y));
 			plr->sector = sect->neighbors[s];
 			break;
 		}
