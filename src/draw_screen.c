3a661148eb06faa8796ecd09a67f2f975b2335a5
Gena <torgo121@gmail.com>
2019-08-18 12:43:07 +0300

    nothing_matter

--- src/draw_screen.c
+++ src/draw_screen.c
@@ -198,7 +198,7 @@ void	render_walls(t_draw_screen_calc *ds, t_sector *sector, t_player plr)
 void	ceil_floor_light(t_draw_screen_calc *ds, t_player *p)
 {
 	/* Calculate the Z coordinate for this point. (Only used for lighting.) */
-	ds->i->z = (int)roundf(((ds->it->x - ds->i->x1) * (ds->f->tz2-ds->f->tz1) / (ds->i->x2-ds->i->x1) + ds->f->tz1) * 40);
+	ds->i->z = (int)roundf(((ds->it->x - ds->i->x1) * (ds->f->tz2-ds->f->tz1) / (ds->i->x2-ds->i->x1) + ds->f->tz1) * 30);
 	ds->i->z = (ds->i->z > 250) ? (250) : (ds->i->z);
 
 	ds->f->perc_light = percentage(250, 0, ds->i->z); //light percent by ds-i->z
