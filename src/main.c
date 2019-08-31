bfe47256c250a2eb105e7894ded521c0881d2c9a
Vladislav <vopolonc@gmail.com>
2019-08-31 14:22:52 +0300

    Added main_e

--- src/main.c
+++ src/main.c
@@ -40,54 +40,6 @@ void	finish_sector_space(t_draw *draw, t_env *env, t_stack **head)
 	draw->key = 0;
 }
 
-void	delete_sector_del(t_env *env, t_draw *draw, t_stack **head)
-{
-	delete_sector_from_list(env, draw);
-	refresh_screen(draw, env, head);
-}
-
-void	player_placement_return(t_env *env, t_draw *draw)
-{
-	SDL_GetMouseState(&env->mouse_x, &env->mouse_y);
-	draw->player = draw->save;
-	draw->place_p.sect_p = draw->save;
-	if(place_player((t_xyf) {.x = (float)env->mouse_x , .y = (float)env->mouse_y}, draw->save))
-	{
-		draw_texture((t_xy) {.x = env->mouse_x, .y = env->mouse_y}, PLAYER, 0xfffffff, env);
-		draw->place_p.x = env->mouse_x / 10;
-		draw->place_p.y = env->mouse_y / 10;
-		select_sector_mode(env, draw, draw->cur_s, 0);
-	}
-}
-
-void	sector_selection_right(t_env *env, t_draw *draw)
-{
-	if (draw->s_count > 1)
-		draw->cur_s += (draw->s_count > draw->cur_s) ? 1 : (-draw->cur_s + 1);
-	else
-		draw->cur_s = 1;
-	draw->save = select_sector_mode(env, draw, draw->cur_s, 0);
-	(draw->save) ? draw_text(1500, 305, ft_itoa(draw->save->ceil), env) : 0 == 0;
-	(draw->save) ? draw_text(1500, 365, ft_itoa(draw->save->floor), env) : 0 == 0;
-}
-
-void	wall_selection_right(t_env *env, t_draw *draw)
-{
-	draw->save = select_sector_mode(env, draw, draw->cur_s, 0);
-	(draw->cur_v > draw->save->walls) ? draw->cur_v = 1 : draw->cur_v;
-	draw->save_v = save_vertex(env, draw->cur_v++, draw->save);
-	draw_text(1500, 305, ft_itoa(draw->save->ceil), env);
-	draw_text(1500, 365, ft_itoa(draw->save->floor), env);
-	(draw->cur_v > 1) ? draw_wall(TEXTURE_COORDS, draw->save_v->texture, env) : 0 == 0;
-}
-
-void	delete_line_backspace(t_env *env, t_draw *draw, t_stack **head)
-{
-	stack_pop(head);
-	refresh_screen(draw, env, head);
-}
-
-
 
 
 t_env *sdl_main_loop(t_env *env)
