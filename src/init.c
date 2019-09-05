9279a91258072665fa50112166e16ba9f1c8e72d
Tal <prokish.tal@icloud.com>
2019-09-05 13:56:37 +0300

    init.c DONE (need check norm on mac)

--- src/init.c
+++ src/init.c
@@ -24,11 +24,9 @@ void	init_sdl(t_game *g)
 	g->sdl.buffer = (int *)malloc(sizeof(int) * W * H);
 	if (!g->sdl.window || !g->sdl.renderer || !g->sdl.buffer || !g->sdl.texture)
 		exit_doom(g);
-
 	g->sdl.textures = (t_textures*)malloc(sizeof(t_textures));
 	SDL_UpdateTexture(g->sdl.texture, NULL, g->sdl.buffer, W * (sizeof(int)));
 	SDL_RenderCopy(g->sdl.renderer, g->sdl.texture, NULL, NULL);
 	SDL_RenderPresent(g->sdl.renderer);
 	SDL_Delay(1000);
-
 }
